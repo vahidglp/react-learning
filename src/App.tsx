4b4c845c387c843182fbcc162533c5f09c7000c2
Auth@1359585131 <vahidglp@gmail.com>
2026-02-20 01:21:31 +0330

    [ADD] add useState with readonly

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
+import { type ReactNode, type MouseEvent, useState } from "react";
 import "./App.css";
 
-function App() {
+type User = {
+  username: string;
+  password: string;
+};
+
+function App(): ReactNode {
+  const [counter, setCounter] = useState(0);
+  const [user, setUser] = useState<Readonly<User>>({
+    username: "vahid golpayegani",
+    password: "1234",
+  });
+
+  const handleButtonClick = (e: MouseEvent<HTMLButtonElement>): void => {
+    console.log("Hello");
+    console.info(e.currentTarget.tagName);
+    handelIncrement();
+  };
+
+  const handelIncrement = (): void => {
+    setCounter((counter) => counter + 1);
+    setCounter((x) => x + 1);
+  };
+
+  const handleButtonChangePassword = (): void => {
+    setUser({
+      ...user,
+      password: "4321",
+    });
+  };
+
   return (
     <>
       <p>Hello Vahid jan</p>
+      <button onClick={handleButtonClick}>Say Hello</button>
+      <p>{counter}</p>
+
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+      <button onClick={handleButtonChangePassword}>Change Password</button>
     </>
   );
 }
